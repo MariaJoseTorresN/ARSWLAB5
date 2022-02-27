ab30e809822f9a331925b7c94eee4e9f030fbcde
majito.200130@gmail.com <majito.200130@gmail.com>
2022-02-27 16:56:36 -0500

    Verificación pendiente

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -5,22 +5,42 @@
  */
 package edu.eci.arsw.blueprints.controllers;
 
-import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
 
 /**
  *
  * @author hcadavid
  */
-public class BlueprintAPIController {
-    
-    
-    
-    
-    
-}
 
+@RestController
+public class BlueprintAPIController {
+
+    @Autowired
+    private BlueprintsServices bps;
+
+    @GetMapping("/blueprints")
+    public ResponseEntity<?> BlueprintGetRecurso() {
+        try {
+            Set<Blueprint> data = bps.getAllBlueprints();
+            return new ResponseEntity<>(data, HttpStatus.ACCEPTED);
+        } catch (BlueprintNotFoundException ex) {
+            Logger.getLogger(BlueprintsServices.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error bla bla bla", HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
